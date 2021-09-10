647c15ad0f390d4f9e76674c59278338ed19071e
Ильдус Садртдинов <ildussadrtdinov@MacBook-Pro-Ildus.local>
2021-09-10 17:32:52 +0300

    fix setup

--- utils/colab_setup.py
+++ utils/colab_setup.py
@@ -9,14 +9,14 @@ def download_github_code(path):
     filename = path.rsplit('/')[-1]
     os.system('shred -u {}'.format(filename))
     os.system(
-        'wget -q https://raw.githubusercontent.com/hse-cs-ami/coursera-intro-dl/main/{} -O {}'.format(path, filename))
+        'wget -q https://raw.githubusercontent.com/hse-cs-ami/coursera-advanced-dl/main/{} -O {}'.format(path, filename))
 
 
 def download_github_release(path):
     filename = path.rsplit('/')[-1]
     os.system('shred -u {}'.format(filename))
     os.system(
-        'wget -q https://github.com/hse-cs-ami/coursera-intro-dl/releases/download/{} -O {}'.format(path, filename))
+        'wget -q https://github.com/hse-cs-ami/coursera-advanced-dl/releases/download/{} -O {}'.format(path, filename))
 
 
 class WeekSetup(ABC):
