26b7fe56b821ebab316d717cce256a82e2908323
Ильдус Садртдинов <ildussadrtdinov@MacBook-Pro-Ildus.local>
2021-10-17 11:57:37 +0300

    week03 fix recording

--- week03-dl-audio/cls/record.py
+++ week03-dl-audio/cls/record.py
@@ -7,7 +7,7 @@ from traitlets import traitlets
 from IPython.display import display, Javascript
 
 
-def record(num_seconds=3, sample_rate=16000, normalize_db=0.1):
+def record(num_seconds=3, sample_rate=16000, normalize_db=0.1, eps_seconds=0.5):
     """
     Record audio via microphone with Javascript
     Based on https://github.com/magenta/ddsp/blob/main/ddsp/colab/colab_utils.py
@@ -37,10 +37,12 @@ def record(num_seconds=3, sample_rate=16000, normalize_db=0.1):
 
     print('Начинается запись на {} секунды...'.format(num_seconds))
     display(Javascript(record_js_code))
-    audio_string = output.eval_js('record(%d)' % (num_seconds * 1000.0))
+    audio_string = output.eval_js('record(%d)' % ((num_seconds + eps_seconds) * 1000.0))
     print('Запись окончена!')
     audio_bytes = base64.b64decode(audio_string.split(',')[1])
-    audio = wav_data_to_samples_pydub(wav_data=audio_bytes, sample_rate=sample_rate, num_channels=1)
+    audio = wav_data_to_samples_pydub(wav_data=audio_bytes, sample_rate=sample_rate,
+                                      normalize_db=normalize_db, num_channels=1)
+    audio = audio[:num_seconds * sample_rate]
     return torch.from_numpy(audio)
 
 
